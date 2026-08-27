55dc1e0dbb5c1bf604d4a99302632f28ac738622
mtl-124 <matteolajara@gmail.com>
2026-08-26 20:23:09 -0500

    "Creación del proyecto Lab02CarritoKotlin y creación de Carrito.kt"

---
+++ Semana02/Lab02CarritoKotlin/app/src/main/java/com/lajara/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.lajara.lab02carritokotlin
+
+fun main() {
+    println("======================================")
+    println("  CARRITO DE COMPRAS - TIENDA TECSUP  ")
+    println("======================================")
+}
