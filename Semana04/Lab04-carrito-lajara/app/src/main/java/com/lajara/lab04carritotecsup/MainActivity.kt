3ca4095ae5575597200f1804830a9df43d6d176d
mtl-124 <matteolajara@gmail.com>
2026-09-21 22:46:10 -0500

    Commit 3: Avance inicial de LazyColumn para las terjetas de los productos

--- Semana04/Lab04-carrito-lajara/app/src/main/java/com/lajara/lab04carritotecsup/MainActivity.kt
+++ Semana04/Lab04-carrito-lajara/app/src/main/java/com/lajara/lab04carritotecsup/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -104,6 +105,9 @@ fun PantallaCarrito(modifier: Modifier = Modifier){
 
         Spacer(modifier = Modifier.height(16.dp))
         Text("Productos: ${productos.size}")
+
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn() { }
     }
 }
 
