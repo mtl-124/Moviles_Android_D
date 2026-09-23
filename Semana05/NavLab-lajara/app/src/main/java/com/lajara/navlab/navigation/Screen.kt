a8ba524125a8e433082bd321d5a41a3dd1d7810a
mtl-124 <matteolajara@gmail.com>
2026-09-22 22:40:52 -0500

    Definición de rutas de navegación - Se crea la clase sellada 'Screen' para gestionar las rutas de la aplicación. - Se definen destinos estáticos: 'Home', 'List' y 'Profile'. - Se implementa el destino 'Detail' con el parametro dinámico 'detail/{itemId}'. - Se agregó una función auxilar 'createRoute' en el objeto Detail para la construcción de la URL.

--- Semana05/NavLab-lajara/app/src/main/java/com/lajara/navlab/navigation/Screen.kt
+++ Semana05/NavLab-lajara/app/src/main/java/com/lajara/navlab/navigation/Screen.kt
@@ -1,4 +1,14 @@
 package com.lajara.navlab.navigation
 
-class Screen {
+sealed class Screen(val route: String) {
+
+    object Home : Screen( route = "home")
+
+    object List : Screen( route = "list")
+
+    object Profile : Screen( route = "profile")
+
+    object Detail : Screen( route = "detail/{itemId}") {
+        fun createRoute(itemId: Int): String = "detail/$itemId"
+    }
 }
