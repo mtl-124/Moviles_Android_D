01197f2c89f39edb3244878a01f8e686311a6b26
mtl-124 <matteolajara@gmail.com>
2026-09-22 21:35:30 -0500

    Creación del proyecto NavBar - Se genera la estructura inicial de los directorios para la aplicación NavBar(Semana 5).

--- Semana05/NavLab-lajara/app/src/main/java/com/lajara/navlab/ui/theme/Theme.kt
+++ Semana05/NavLab-lajara/app/src/main/java/com/lajara/navlab/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.lajara.lab04carritotecsup.ui.theme
+package com.lajara.navlab.ui.theme
 
 import android.app.Activity
 import android.os.Build
@@ -14,21 +14,30 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80   
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40,
-    surfaceVariant = SurfaceVariantLight,
-    background = BackgroundWhite
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
 )
 
 @Composable
-fun Lab04CarritoTecsupTheme(
-    darkTheme: Boolean = false,
-    dynamicColor: Boolean = false,
+fun NavLabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
